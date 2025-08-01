334dca634946fc4e503749bd0e1b20f38b443b46
Aanand Rai <aanand.rai@gigaforce.io>
2025-08-01 17:11:38 +0530

    adding Joi , schema validation

---
+++ Backend/src/middlewares/inputValidater.js
@@ -0,0 +1,21 @@
+import Joi from "joi"
+import { ApiError } from "../utills/ApiError.js"
+import { asyncHandler } from "../utills/asyncHandler.js"
+
+
+const userSchema=Joi.object({
+    fullname: Joi.object({
+        firstname: Joi.string().min(3).required(),
+        lastname: Joi.string().min(3).optional(),
+    }).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(5).required(),
+})
+
+const validateUser=asyncHandler((req,res,next)=>{
+    const {error}=userSchema.validate(req.body)
+    if(error) throw new ApiError(400,error.details[0].message ||"Input data Validation fails")
+    next()
+})
+
+export{validateUser}
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,10 +1,10 @@
 import express from "express"
 import { registerUser } from "../controllers/user.controller.js"
-
+import { validateUser } from "../middlewares/inputValidater.js"
 const router=express.Router()
 
 router.route("/register")
-    .post(registerUser)
+    .post(validateUser,registerUser)
 
 
 
